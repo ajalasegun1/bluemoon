c5e3ed9e48befa37f4c9e6b98341076f3660a39c
Segun Ajala <ajalasegun1@gmail.com>
2023-03-12 10:04:21 +0100

    Home screen snapshot now runs after using react native testing library

--- __tests__/Home-test.tsx
+++ __tests__/Home-test.tsx
@@ -5,11 +5,15 @@
 import 'react-native';
 import React from 'react';
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
+import {render, screen, fireEvent} from '@testing-library/react-native';
 import Home from '../src/screens/Home';
 
 test('Home Screen Snapshot', async () => {
   let props: any;
-  const tree = renderer.create(<Home {...props} />).toJSON();
+  //   const tree = renderer.create(<Home {...props} />).toJSON();
+  //   expect(tree).toMatchSnapshot();
+
+  const tree = render(<Home {...props} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
